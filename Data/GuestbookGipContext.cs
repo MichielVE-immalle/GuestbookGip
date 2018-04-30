b55641604118ccd3aa7865ecb6f9acce10265d12
Michiel Van Eester <michiel.vaneester@immalle.be>
2018-04-30 14:33:18 +0200

    Controller: Commentscontroller added

---
+++ Data/GuestbookGipContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GuestbookGip.Models
+{
+    public class GuestbookGipContext : DbContext
+    {
+        public GuestbookGipContext (DbContextOptions<GuestbookGipContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GuestbookGip.Models.Comment> Comment { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,9 @@ namespace GuestbookGip
             services.AddTransient<IEmailSender, EmailSender>();
 
             services.AddMvc();
+
+            services.AddDbContext<GuestbookGipContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("GuestbookGipContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
