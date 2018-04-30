980849941640b6718c810efcd945c824ff2a9b80
Michiel Van Eester <michiel.vaneester@immalle.be>
2018-04-30 14:21:09 +0200

    Model: Comment added

---
+++ Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuestbookGip.Models
+{
+    public class Comment
+    {
+        public int ID { get; set; }
+        public string Message { get; set; }
+        public DateTime Date { get; set; }
+        public string Name { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
